01f00a79b74493a5c08c3c00d8d135a682259741
sugapriyanganesh <144574881+sugapriyanganesh@users.noreply.github.com>
2023-09-10 08:59:57 +0530

    Add files via upload

---
+++ challenge unit-1/main.py
@@ -0,0 +1,15 @@
+# Function to check if a year is a leap year
+def is_leap_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
+
+# Input year from the user
+year = int(input("Enter a year: "))
+
+# Check if it's a leap year and display the result
+if is_leap_year(year):
+    print(year, "is a leap year.")
+else:
+    print(year, "is not a leap year.")
